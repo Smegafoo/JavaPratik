ac6753c76bb108ddbe9894caa4df0cfc1f67f9c7
Emir <89991170+Smegafoo@users.noreply.github.com>
2023-03-06 22:21:52 +0300

    Add files via upload
    For döngüsü ile Üslü sayılar

--- Uslusayi.java
+++ Uslusayi.java
@@ -3,20 +3,18 @@ import java.util.Scanner;
 
 public class Uslusayi {
     public static void main(String[] args) {
-        int n,k;
         Scanner in = new Scanner(System.in);
+        int n,k;
+        int result=1;
         System.out.print("Üssü alınacak sayıyı giriniz :");
-        n =in.nextInt();
+        n=in.nextInt();
         System.out.print("Üs olacak sayıyı giriniz :");
-        k = in.nextInt();
-        int total=1;
-        int i=1;
-        while(i<=k){
-            i++;
-            total *=n;
-        }
-        System.out.println("Cevabınız :"+total);
+        k=in.nextInt();
+        for(int i=1;i<=k;i++){
+            result *=n;
 
+        }
+        System.out.println(result);
     }
 
 }
